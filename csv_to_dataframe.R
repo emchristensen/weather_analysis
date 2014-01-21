a56f18cdfa167817fee7a4fde02088b24e1cc302
emchristensen <erica.christensen@weecology.org>
2014-01-21 14:36:36 -0500

    Adding a new R script

---
+++ csv_to_dataframe.R
@@ -0,0 +1,7 @@
+create_dataframe = function(datafile) {
+  #converts data in csv file to data frame form
+  rawdata = read.csv(paste(datafile,sep=''),head=T,sep=',')
+  dataframe = data.frame(rawdata)
+  return(dataframe)
+}
+
