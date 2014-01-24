d4f8b922435dcee9711e62aed5d1d86714406647
emchristensen <erica.christensen@weecology.org>
2014-01-24 13:24:50 -0500

    finished code, added write to csv for end result

--- weather_monthly_summary.R
+++ weather_monthly_summary.R
@@ -12,7 +12,7 @@ monthly_summary_from_hourly = function(dataframe) {
   #meantemp = aggregate(dataframe$TempAir,by=list(dataframe$Month,dataframe$Year),FUN=mean)
   maxtemp = aggregate(dataframe$TempAir,by=list(dataframe$Month,dataframe$Year),FUN=max)
   mintemp = aggregate(dataframe$TempAir,by=list(dataframe$Month,dataframe$Year),FUN=min)
-  #meanhumid = aggregate(dataframe$RelHumid,by=list(dataframe$Monh,dataframe$Year),FUN=mean)
+  #meanhumid = aggregate(dataframe$RelHumid,by=list(dataframe$Month,dataframe$Year),FUN=mean)
   
   month = sumprecip$Group.1
   year = sumprecip$Group.2
@@ -33,7 +33,7 @@ monthly_summary_from_daily = function(dataframe) {
   #meantemp = aggregate(dataframe$TempAir,by=list(dataframe$Month,dataframe$Year),FUN=mean)
   maxtemp = aggregate(dataframe$TempAirMax,by=list(dataframe$Month,dataframe$Year),FUN=max)
   mintemp = aggregate(dataframe$TempAirMin,by=list(dataframe$Month,dataframe$Year),FUN=min)
-  #meanhumid = aggregate(dataframe$RelHumid,by=list(dataframe$Monh,dataframe$Year),FUN=mean)
+  #meanhumid = aggregate(dataframe$RelHumid,by=list(dataframe$Month,dataframe$Year),FUN=mean)
   
   month = sumprecip$Group.1
   year = sumprecip$Group.2
@@ -47,6 +47,17 @@ monthly_summary_from_daily = function(dataframe) {
   return(data.frame(month,year,sumprecip,meanprecip,maxtemp,mintemp))
 }
 
+combine_two_datasets = function(dataframe1,dataframe2) {
+  #combines the two monthly-summary data sets into one
+  year = c(dataframe1$year,dataframe2$year)
+  month = c(dataframe1$month,dataframe2$month)
+  sumprecip = c(dataframe1$sumprecip,dataframe2$sumprecip)
+  meanprecip = c(dataframe1$meanprecip,dataframe2$meanprecip)
+  maxtemp = c(dataframe1$maxtemp,dataframe2$maxtemp)
+  mintemp = c(dataframe1$mintemp,dataframe2$mintemp)
+  
+  return(data.frame(year,month,sumprecip,meanprecip,maxtemp,mintemp))
+}
 
 weathfile = "data/Hourly_PPT_mm_1989_present_fixed_withgaps.csv"
 weathframe = create_dataframe(weathfile)
@@ -54,11 +65,12 @@ weathframe = create_dataframe(weathfile)
 oldweathfile = "data/Daily_weather_1980_89.csv"
 oldwframe = create_dataframe(oldweathfile)
 
-monthly1 = monthly_summary_from_hourly(weathframe)
-monthly2 = monthly_summary_from_daily(oldwframe)
+monthly1 = monthly_summary_from_daily(oldwframe)
+monthly2 = monthly_summary_from_hourly(weathframe)
 
-monthly2 = monthly2[1:113,] #remove last two entries so two halves of series match up
-year = c(monthly2$year,monthly1$year)
-month = c(monthly2$month,monthly1$month)
-sumprecip = c(monthly2$sumprecip,monthly1$sumprecip)
-monthly = data.frame(year,month,sumprecip)
+monthly1 = monthly1[1:113,] #remove last two entries so two halves of series match up
+
+monthly = combine_two_datasets(monthly1,monthly2)
+
+outfile = "data/Monthly_ppt_1980_2013.csv"
+write.csv(monthly,file=outfile,row.names=F)
