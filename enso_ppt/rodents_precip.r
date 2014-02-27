a1e3283e4d11942b44853efe4683e4bb9c0fb951
emchristensen <erica.christensen@weecology.org>
2014-02-27 15:14:40 -0500

    script for making plots

---
+++ enso_ppt/rodents_precip.r
@@ -0,0 +1,4 @@
+# this script is for analyzing the connection between precip and rodent abundance
+
+source('portal_weather/enso_ppt/yearly_rodents.r')
+source('portal_weather/yearly_winter_precip.r')
