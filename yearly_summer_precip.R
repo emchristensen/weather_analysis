ada22c719e11730ee8ab62599aa9e9f2a6a7748a
emchristensen <erica.christensen@weecology.org>
2015-04-09 15:06:16 -0600

    updated scripts for looking at seasonal ppt patterns

--- yearly_summer_precip.R
+++ yearly_summer_precip.R
@@ -1,12 +1,31 @@
-#load function for reading csv data
-source('portal_weather/csv_to_dataframe.r')
+# This script takes monthly weather summary and writes a new file of 
+# total summer precip.
+# Written by EMC 3/11/15
+#
+# Input:
+#     Monthly_ppt_1980_present.csv
+#         year, month, sumprecip, maxtemp, mintemp
+#
+# Output:
+#     
+#
+# Scripts called:
+#     weather_daily_summary.r     combines hourly dataset (1989-present) and daily dataset (1980-89)
+
+
+# set working directory and run prelim scripts
+setwd('C:/Users/EC/git_dir/')
+source('portal_weather/weather_monthly_summary.r')
+
+# =============================================================================================
+# functions
 
 yearly_summer_precip = function(dataframe) {
   #sums precipitation for summer months (June-Sept), returns yearly total
   year= vector()
   ppt = vector()
-  for (yr in 1980:2012) {
-    yr_mo = list(c(yr,6),c(yr,7),c(yr,8),c(yr,9))
+  for (yr in 1980:2014) {
+    yr_mo = list(c(yr,5),c(yr,6),c(yr,7),c(yr,8),c(yr,9))
     summ_ppt = vector()
     for (i in 1:length(dataframe$year)) {
       if (is.element(list(c(dataframe$year[i],dataframe$month[i])),yr_mo)) {
@@ -19,7 +38,18 @@ yearly_summer_precip = function(dataframe) {
   return(data.frame(year,ppt))
 }
 
-weathfile = "data/Monthly_ppt_1980_2013_nona.csv"
-weathframe = csv_to_dataframe(weathfile)
+# read in daily data, aggregate  by month excluding NAs, only take data from months with <10 days NA
+weathframe = read.csv("data/Daily_weather_1980_present_fixed_withgaps.csv")
+monthly = aggregate(weathframe$Precipitation,by=list(weathframe$Year,weathframe$Month),FUN=sum,na.rm=T)
+names(monthly) = c('year','month','sumprecip')
 
-summer_ppt = yearly_summer_precip(weathframe)
+nas = weathframe[is.na(weathframe$Precipitation),]
+nacount = aggregate(nas$Precipitation,by=list(nas$Year,nas$Month),FUN=length)
+names(nacount) = c('year','month','na')
+
+monthly = merge(monthly,nacount,by=c('year','month'),all=T)
+monthly[is.na(monthly$na),4] = 0
+# make precip NA if >10 days missing
+monthly[monthly$na>10,3] = NA
+
+summer_ppt = yearly_summer_precip(monthly)
--- yearly_winter_precip.R
+++ yearly_winter_precip.R
@@ -1,12 +1,31 @@
-#load function for reading csv data
-source('portal_weather/csv_to_dataframe.r')
+# This script takes monthly weather summary and writes a new file of 
+# total winter precip.
+# Written by EMC 3/11/15
+#
+# Input:
+#     Monthly_ppt_1980_present.csv
+#         year, month, sumprecip, maxtemp, mintemp
+#
+# Output:
+#     
+#
+# Scripts called:
+#     weather_daily_summary.r     combines hourly dataset (1989-present) and daily dataset (1980-89)
+
+
+# set working directory and run prelim scripts
+setwd('C:/Users/EC/git_dir/')
+source('portal_weather/weather_monthly_summary.r')
+
+# =====================================================================================================
+# functions
 
 yearly_winter_precip = function(dataframe) {
   #sums precipitation for winter months (Dec-March), returns yearly total
   year= vector()
   ppt = vector()
-  for (yr in 1980:2012) {
-    yr_mo = list(c(yr,12),c(yr+1,1),c(yr+1,2),c(yr+1,3))
+  for (yr in 1980:2014) {
+    yr_mo = list(c(yr-1,10),c(yr-1,11),c(yr-1,12),c(yr,1),c(yr,2),c(yr,3),c(yr,4))
     win_ppt = vector()
     for (i in 1:length(dataframe$year)) {
       if (is.element(list(c(dataframe$year[i],dataframe$month[i])),yr_mo)) {
@@ -19,8 +38,18 @@ yearly_winter_precip = function(dataframe) {
   return(data.frame(year,ppt))
 }
 
-weathfile = "data/Monthly_ppt_1980_2013_nona.csv"
-weathframe = csv_to_dataframe(weathfile)
+# read in daily data, aggregate  by month excluding NAs, only take data from months with <10 days NA
+weathframe = read.csv("data/Daily_weather_1980_present_fixed_withgaps.csv")
+monthly = aggregate(weathframe$Precipitation,by=list(weathframe$Year,weathframe$Month),FUN=sum,na.rm=T)
+names(monthly) = c('year','month','sumprecip')
 
-winter_ppt = yearly_winter_precip(weathframe)
+nas = weathframe[is.na(weathframe$Precipitation),]
+nacount = aggregate(nas$Precipitation,by=list(nas$Year,nas$Month),FUN=length)
+names(nacount) = c('year','month','na')
 
+monthly = merge(monthly,nacount,by=c('year','month'),all=T)
+monthly[is.na(monthly$na),4] = 0
+# make precip NA if >10 days missing
+monthly[monthly$na>10,3] = NA
+
+winter_ppt = yearly_winter_precip(monthly)
